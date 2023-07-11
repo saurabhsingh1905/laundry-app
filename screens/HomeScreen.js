f98ef013e6a384579d54687d98fd3ca926cb400e
saurabh singh <singhsaurabh1905@gmail.com>
2023-07-11 06:40:06 +0530

    location done

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,74 +1,137 @@
-import { StyleSheet, Text, View,SafeAreaView } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import * as Location from "expo-location"
-import { Alert } from 'react-native'
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Pressable,
+  Image,
+  TextInput,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import * as Location from "expo-location";
+import { Alert } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+import Carousel from "../components/Carousel";
 
 const HomeScreen = () => {
+  const [displayCurrentAddress, setdisplayCurrentAddress] = useState(
+    "loading your location"
+  );
+  const [locationServicesEnabled, setlocationServicesEnabled] = useState(false);
 
-    const[displayCurrentAddress,setdisplayCurrentAddress]= useState("loading your location")
-    const[locationServicesEnabled,setlocationServicesEnabled]=useState(false)
+  // useEffect to check wheather location is enabled in phone or not
+  useEffect(() => {
+    checkIfLocationEnabled();
+    getCurrentLocation();
+  }, []);
 
-    // useEffect to check wheather location is enabled in phone or not
-    useEffect(()=>{
-        checkIfLocationEnabled();
-        getCurrentLocation();
-    }, [])
+  const checkIfLocationEnabled = async () => {
+    let enabled = await Location.hasServicesEnabledAsync();
+    if (!enabled) {
+      Alert.alert(
+        "Location services not enabled",
+        "Please enable the location services",
+        [
+          {
+            text: "Cancel",
+            onPress: () => console.log("Cancel Pressed"),
+            style: "cancel",
+          },
+          { text: "OK", onPress: () => console.log("OK Pressed") },
+        ],
+        { cancelable: false }
+      );
+    } else {
+      setlocationServicesEnabled(enabled);
+    }
+  };
+  const getCurrentLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
 
-    const checkIfLocationEnabled = async()=>{
-        let enabled = await Location.hasServicesEnabledAsync();
-        if(!enabled){
-            Alert.alert(
-                "Location services not enabled",
-                "Please enable the Location services",
-                [
-                    {
-                        text:"Cancel",
-                        onPress:()=> console.log("cancle pressed"),
-                        style:"cancel"
-                    },
-                    {
-                        text:"OK",
-                        onPress:()=> console.log("OK pressed ")
-                    },
-                ],
-                    {cancelable:false}
-            )
-        }else{
-            setlocationServicesEnabled(enabled)
-        }
+    if (status !== "granted") {
+      Alert.alert(
+        "Permission denied",
+        "allow the app to use the location services",
+        [
+          {
+            text: "Cancel",
+            onPress: () => console.log("Cancel Pressed"),
+            style: "cancel",
+          },
+          { text: "OK", onPress: () => console.log("OK Pressed") },
+        ],
+        { cancelable: false }
+      );
     }
 
-    //getting lattitude & longitude
-    const getCurrentLocation = async()=>{
-        let{status} = await Location.requestForegroundPermissionsAsync();
+    const { coords } = await Location.getCurrentPositionAsync();
+     console.log(coords)
+    if (coords) {
+      const { latitude, longitude } = coords;
 
-        if (status!== granted){
-            Alert.alert(
-                " Permission denied",
-                "Allow the app to use Location services",
-                [
-                    {
-                        text:"Cancel",
-                        onPress:()=> console.log("cancle pressed"),
-                        style:"cancel"
-                    },
-                    {
-                        text:"OK",
-                        onPress:()=> console.log("OK pressed ")
-                    },
-                ],
-                    {cancelable:false}
-            ) 
-        }
+      let response = await Location.reverseGeocodeAsync({
+        latitude,
+        longitude,
+      });
+
+       console.log(response)
+
+      for (let item of response) {
+        let address = `${item.name} ${item.city} ${item.postalCode}`;
+        setdisplayCurrentAddress(address);
+      }
     }
+  };
 
   return (
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
-    </SafeAreaView>
-  )
-}
+    <ScrollView
+    style={{ backgroundColor: "#F0F0F0", flex: 1, marginTop: 30 }}
+  >
+       {/* Location and Profile */}
+       <View
+          style={{ flexDirection: "row", alignItems: "center", padding: 10 }}
+        >
+          <MaterialIcons name="location-on" size={30} color="#fd5c63" />
+          <View>
+            <Text style={{ fontSize: 18, fontWeight: "600" }}>Home</Text>
+            <Text>{displayCurrentAddress}</Text>
+          </View>
 
-export default HomeScreen
+          <Pressable onPress={() => navigation.navigate("Profile")} style={{ marginLeft: "auto", marginRight: 7 }}>
+            <Image
+              style={{ width: 40, height: 40, borderRadius: 20 }}
+              source={{
+                uri: "https://lh3.googleusercontent.com/ogw/AAEL6sh_yqHq38z35QMy5Fnb8ZIxicdxCIVM9PeBD2j-=s64-c-mo",
+              }}
+            />
+          </Pressable>
+        </View>
 
-const styles = StyleSheet.create({})
+      {/* Search Bar */}
+      <View
+          style={{
+            padding: 10,
+            margin: 10,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+            borderWidth: 0.8,
+            borderColor: "#C0C0C0",
+            borderRadius: 7,
+          }}
+        >
+          <TextInput placeholder="Search for items or More" />
+          <Feather name="search" size={24} color="#fd5c63" />
+        </View>
+
+      {/* Image carousel */}
+      <Carousel />
+
+    </ScrollView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
